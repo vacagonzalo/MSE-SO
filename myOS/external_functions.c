afc73b9b4a4c9476abae81a349177f645125ec85
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-07-17 16:31:11 -0300

    add PendsSV_Handler

---
+++ myOS/external_functions.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+
+uint32_t os_get_running_task(void)
+{
+	return -1;
+}
+
+void delay(uint32_t milliseconds)
+{
+
+}
+
+int32_t getNextContext(int32_t current_context)
+{
+
+	return -1;
+}
+
+void schedule(void)
+{
+
+}
+
+void SysTick_Handler(void)
+{
+
+}
+
+int start_os(void)
+{
+	return -1;
+}
+
+void * idle_hook(void * p)
+{
+	return 0;
+}
---
+++ myOS/myOS.h
@@ -0,0 +1,13 @@
+/*
+ * myOS.h
+ *
+ *  Created on: Jul 17, 2022
+ *      Author: gonzalo
+ */
+
+#ifndef MYOS_H_
+#define MYOS_H_
+
+
+
+#endif /* MYOS_H_ */
